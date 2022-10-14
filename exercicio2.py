267555ae927f60d804f0d98e573d412f13c95b74
Antonio Trigo <antonio.trigo@gmail.com>
2022-10-15 00:23:24 +0100

    Create exercicio2.py

---
+++ exercicio2.py
@@ -0,0 +1,4 @@
+"""
+Escreva uma função cria_lista_multiplos que recebe um número inteiro positivo, e devolve uma lista com os dez primeiros múltiplos desse número. A sua função deve testar se o seu argumento é um número inteiro positivo e dar uma mensagem de erro adequada em caso contrário. Considere que zero é múltiplo de todos os números.
+"""
+
